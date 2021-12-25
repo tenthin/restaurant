bc695bd8d14272df5a4bd762fcdc0bbd30c9eaf8
Tenzin <tenthinten@gmail.com>
2021-12-25 12:08:25 +0530

    styling the site

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { PureComponent } from 'react/cjs/react.production.min'
+import Restaurant from './component/Basics/Restaurant'
+
+const App = () => {
+  return <Restaurant />  //this is how you call component in other PureComponent
+}
+
+export default App
---
+++ src/component/Basics/Restaurant.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Restaurant = () => {
+    return( 
+        <> 
+            <h1>Hello Restaurant</h1>
+        </>
+    )
+}
+
+export default Restaurant
