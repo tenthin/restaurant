3ac9468e66d918404c5d1d94d6f4e90d66ff87d1
Tenzin <tenthinten@gmail.com>
2021-12-25 18:05:09 +0530

    changing name of dishes

--- src/component/Basics/MenuCard.js
+++ src/component/Basics/MenuCard.js
@@ -8,12 +8,14 @@ const MenuCard = ({ menuData }) => {
                 {menuData.map((curElem) => {
                     return(
                         <>
-                            <div className="card-container">
+                            <div className="card-container" key={curElem.id}>
                                 <div className="card">
                                 <div className="card-body">
-                                    <span className="card-number card-circle subtle">1</span>
-                                    <span className="card-author subtle">Breakfast</span>
-                                    <h2 className="card-title"> Maggi </h2>
+                                    <span className="card-number card-circle subtle">
+                                        {curElem.id}
+                                    </span>
+                                    <span className="card-author subtle">{curElem.name}</span>
+                                    <h2 className="card-title"> {curElem.name} </h2>
                                     <span className="card-description subtle">
                                     I love magge. dfajf jalsdjfjew; asdjflka jaldjfl fa f adjfj asdf.
                                     ajsdjfj adf fafjafj .dsfjadfj alf.
