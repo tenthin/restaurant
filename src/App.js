1d6662c585c3d7d0e0feacaaf0e7b85fcf0a0209
Tenzin <tenthinten@gmail.com>
2021-12-27 19:10:01 +0530

    increment and decrement buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { PureComponent } from 'react/cjs/react.production.min'
 import Restaurant from './component/Basics/Restaurant'
+import UseState from './component/Hooks/useState'
 
 const App = () => {
-  return <Restaurant />  //this is how you call component in other PureComponent
+  return <UseState />  //this is how you call component in other PureComponent
 }
 
 export default App
